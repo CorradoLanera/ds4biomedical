452fb52f278371418f6211a60e3eb49194c72f54
Corrado <corrado.lanera@gmail.com>
2021-09-27 17:28:35 +0200

    Python and dependencies

--- dev/00-setup.R
+++ dev/00-setup.R
@@ -1,6 +1,15 @@
+# Packages required for part 1
 install.packages(c("tidyverse", "remotes", "writexl"),
                  dependencies = TRUE)
 remotes::install_github("higgi13425/medicaldata")
+
+
+# Additional packages reqired for part 2
+install.packages(c(
+    "RSQLite", "tidymodels", "janitor", "skimr", "rmarkdown", "knitr"
+))
+remotes::install_github("thomasp85/patchwork")
+
 
 renv::status()
 renv::snapshot()
